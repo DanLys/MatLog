7680e5cdb10500e77eb9d46c270a1b7a3cca8d34
Руслан <ruslan@MacBook-Pro-Ruslan.local>
2021-02-28 16:25:53 +0300

    fibo parabol

---
+++ src/main/java/method/optimisation/data/fibonacchi/Fibonachi.java
@@ -0,0 +1,94 @@
+public class Fibonachi {
+
+    public static void main(String[] args) {
+        int n = 30;
+        System.out.println(fibonacchiOptimisation(1, 5, 0.00001, n));
+    }
+
+    private static double[] a, b, fibArr;
+    private static double x1, x2;
+    private static int k = 0, ind = 0;
+    private static final double DOUBLE_EPSILON = 0.000000001;
+
+    private static boolean doubleEquals(double x1, double x2) {
+        return Math.abs(x2 - x1) < DOUBLE_EPSILON;
+    }
+
+    private static double functionResult(double x) {
+//        return Math.exp(x) - 2 * Math.pow(x, 2);
+        return Math.sin(x) + 1 / x;
+    }
+
+    private static void fillFibArr(final int n) {         // числа фибоначчи предпроцессинг
+        fibArr = new double[n];
+        fibArr[0] = 1;
+        fibArr[1] = 1;
+        long x1 = 1, x2 = 1, res;
+        for (int i = 2; i < n; i++) {
+            res = x1 + x2;
+            x1 = x2;
+            x2 = res;
+            fibArr[i] = Double.parseDouble(Long.toString(res));
+        }
+    }
+
+    private static double fib(final int n) {        // get value fib
+        return fibArr[n - 1];
+    }
+
+    private static int minusOnePower(int power) {
+        if (power % 2 == 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+
+    public static double fibonacchiOptimisation(double a1, double b1, double eps, int n) {    // main func
+        fillFibArr(n);
+        a = new double[n + 1];  // fix ind
+        b = new double[n + 1];
+        a[0] = a1;
+        b[0] = b1;
+        double l1 = b1 - a1;
+        double l2 = fib(n - 1) / fib(n) * l1 + minusOnePower(n) * eps / fib(n);
+        x2 = a1 + l2;
+        stepOne(n);
+        return (a[ind] + b[ind]) / 2;
+    }
+
+    private static void stepOne(int n) {    // step 4
+        x1 = a[ind] + (b[ind] - x2);
+        double fX1 = functionResult(x1),
+                fX2 = functionResult(x2);
+        if (fX1 < fX2) {
+            if (x1 < x2) {
+                b[++ind] = x2;
+                a[ind] = a[ind - 1];
+            } else {    // x1 > x2
+                a[++ind] = x2;
+                b[ind] = b[ind - 1];
+            }
+            x2 = x1;
+            fX2 = fX1;
+        } else {
+            if (x1 < x2) {
+                a[++ind] = x1;
+                b[ind] = b[ind - 1];
+            } else {    // x1 > x2
+                b[++ind] = x1;
+                a[ind] = a[ind - 1];
+            }
+        }
+        stepTwo(n);
+    }
+
+    private static void stepTwo(int n) {
+       k++;
+       if (k < n) {
+           stepOne(n);
+       } // else finish
+    }
+}
+
---
+++ src/main/java/method/optimisation/data/parabola/Parabola.java
@@ -0,0 +1,103 @@
+public class Parabola {
+    public static void main(String[] args) {
+        System.out.println(launchParabolaMethod(4, 7, 0.0001));
+    }
+
+    private static class TripleF {
+        public static double fX1, fX2, fX3;    // значения функции в точках x1, x2, x3
+    }
+
+    private static class TripleX {
+        public double x1, x2, x3;
+
+        private TripleX(double x1, double x2, double x3) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.x3 = x3;
+        }
+    }
+
+    private static double functionResult(double x) {
+//        return 0.2 * x * Math.log(x) + Math.pow((x - 2.3), 2);
+        return Math.sin(x) + 1 / x; // 4.7566019161693704
+    }
+
+    public static double launchParabolaMethod(double a, double b, double eps) {
+        return parabolaMethod(new TripleX(a, chooseX2(a, b), b), eps);
+    }
+
+    private static double parabolaMethod(TripleX tripleX, double eps) {  // main parabola
+        double prevXmin = stepOne(tripleX);
+        TripleX tripleX1 = stepThree(tripleX, prevXmin);
+        double xMin;
+        while (true) {
+            xMin = stepOne(tripleX1);
+            if (stepTwo(prevXmin, xMin, eps)) {
+                break;
+            }
+            prevXmin = xMin;
+            tripleX1 = stepThree(tripleX1, xMin);
+        }
+        return xMin;
+    }
+
+    private static double chooseX2(double a, double b) {  // методом золотого сечения
+        double x21 = a + (3 - Math.sqrt(5)) / 2 * (b - a),
+                x22 = a + (Math.sqrt(5) - 1) / 2 * (b - a),
+                fB = functionResult(b),
+                fX21 = functionResult(x21),
+                fX22 = functionResult(x22);
+        if (fX21 <= fB) return x21;
+        if (fX22 <= fB) return x22;
+        return chooseX2(x22, b);
+    }
+
+    private static double stepOne(TripleX tripleX) {
+        TripleF.fX1 = functionResult(tripleX.x1);
+        TripleF.fX2 = functionResult(tripleX.x2);
+        TripleF.fX3 = functionResult(tripleX.x3);
+        double a1 = (TripleF.fX2 - TripleF.fX1) / (tripleX.x2 - tripleX.x1),
+                a2 = 1 / (tripleX.x3 - tripleX.x2) * ((TripleF.fX3 - TripleF.fX1) / (tripleX.x3 - tripleX.x1)
+                 - (TripleF.fX2 - TripleF.fX1) / (tripleX.x2 - tripleX.x1)),
+                xMin = 0.5 * (tripleX.x1 + tripleX.x2 - a1 / a2);
+        return xMin;
+    }
+
+    private static boolean stepTwo(double prevMin, double xMin, double eps) {
+        return Math.abs(prevMin - xMin) <= eps;
+    }
+
+    private static TripleX stepThree(TripleX tripleX, double xMin) {  // define new x1, x2, x3
+        double fXmin = functionResult(xMin);
+        if (tripleX.x1 < xMin
+                && xMin < tripleX.x2
+                && tripleX.x2 < tripleX.x3) {
+            if (fXmin >= TripleF.fX2) {
+                tripleX.x1 = xMin;
+                TripleF.fX1 = fXmin;
+            } else {
+                tripleX.x3 = tripleX.x2;
+                tripleX.x2 = xMin;
+                TripleF.fX3 = TripleF.fX2;
+                TripleF.fX2 = fXmin;
+            }
+        }
+
+        if (tripleX.x1 < tripleX.x2
+                && tripleX.x2 < xMin
+                && xMin < tripleX.x3) {
+            if (TripleF.fX2 >= fXmin) {
+                tripleX.x1 = tripleX.x2;
+                TripleF.fX1 = TripleF.fX2;
+                tripleX.x2 = xMin;
+                TripleF.fX2 = fXmin;
+            } else {
+                tripleX.x3 = xMin;
+                TripleF.fX3 = fXmin;
+            }
+        }
+        return tripleX;
+    }
+}
+
+
