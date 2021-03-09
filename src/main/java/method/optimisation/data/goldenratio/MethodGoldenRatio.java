97b5ae4f9e60ce1cf75c71b4a2e8ab89b3f112fc
DanLys <lyskin-2013@mail.ru>
2021-03-09 22:01:06 +0300

    add service for GoldenRatio

--- src/main/java/method/optimisation/data/goldenratio/MethodGoldenRatio.java
+++ src/main/java/method/optimisation/data/goldenratio/MethodGoldenRatio.java
@@ -1,14 +1,17 @@
 package method.optimisation.data.goldenratio;
 
-public class MethodGoldenRatio {
-    private final static double EPSILON = 1E-9;
+import org.springframework.stereotype.Component;
 
-    private static double f(double x) {
+@Component
+public class MethodGoldenRatio {
+    private final double EPSILON = 1E-9;
+
+    private double f(double x) {
         return 0.2 * x * Math.log10(x) + Math.pow((x - 2.3), 2);
     }
 
     // Return x : f(x) = min
-    private static double goldenRatio(double a, double b, int iterations) {
+    public double goldenRatio(double a, double b, int iterations) {
         // Step 1
         double x1 = a + (3 - Math.sqrt(5)) / 2 * (b - a);
         double x2 = a + (Math.sqrt(5) - 1) / 2 * (b - a);
@@ -36,9 +39,5 @@ public class MethodGoldenRatio {
         }
         // Step 4
         return (a + b) / 2;
-    }
-
-    public static void main(String[] args) {
-        System.out.print(f(goldenRatio(0.5, 2.5, 100)));
     }
 }
