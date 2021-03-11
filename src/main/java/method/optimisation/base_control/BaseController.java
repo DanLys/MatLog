5561abc893fb6dc97c3542a7014fc73f9f2467ea
Руслан <ruslan@MacBook-Pro-Ruslan.local>
2021-03-11 16:54:38 +0300

    html page

--- src/main/java/method/optimisation/base_control/BaseController.java
+++ src/main/java/method/optimisation/base_control/BaseController.java
@@ -3,10 +3,7 @@ package method.optimisation.base_control;
 import method.optimisation.attributes.MethodsType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Danil Lyskin at 15:02 25.02.2021
